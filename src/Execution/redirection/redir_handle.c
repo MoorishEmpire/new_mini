00b5c8ade2851a0ecd864d0b0bb2c071e492b972
Mohamed El Idrissi <moel-idr@c2r9p1.1337.ma>
2025-10-17 20:12:44 +0100

    heredoc hanging fixed

--- src/Execution/redirection/redir_handle.c
+++ src/Execution/redirection/redir_handle.c
@@ -6,7 +6,7 @@
 /*   By: moel-idr <moel-idr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 00:13:58 by moel-idr          #+#    #+#             */
-/*   Updated: 2025/10/13 20:29:00 by moel-idr         ###   ########.fr       */
+/*   Updated: 2025/10/17 18:11:19 by moel-idr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,19 @@ int apply_redirections(t_cmd *cmd, char **env)
     i = 0;
     while(cmd->redirect[i])
     {
+        if(ft_strncmp(cmd->redirect[i],"<<",2) == 0)
+        {
+            if(last_heredoc_fd != -1)
+                close(last_heredoc_fd);
+            last_heredoc_fd = handle_heredoc(cmd->file[i],env,cmd,i);
+            if(last_heredoc_fd == -1)
+                return(-1);
+        }
+        i++;  
+    }
+    i = 0;
+    while(cmd->redirect[i])
+    {
         if(ft_strncmp(cmd->redirect[i],">",2) == 0)
             if(handle_out(cmd->file[i],0, cmd) == -1)
                 return(-1);
@@ -29,14 +42,6 @@ int apply_redirections(t_cmd *cmd, char **env)
         if(ft_strncmp(cmd->redirect[i],"<",2) == 0)
             if(handle_in(cmd->file[i], cmd) == -1)
                 return(-1);
-        if(ft_strncmp(cmd->redirect[i],"<<",2) == 0)
-        {
-            if(last_heredoc_fd != -1)
-                close(last_heredoc_fd);
-            last_heredoc_fd = handle_heredoc(cmd->file[i],env,cmd,i);
-            if(last_heredoc_fd == -1)
-                return(-1);
-        }
         i++;  
     }
     if(last_heredoc_fd != -1)
